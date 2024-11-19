cc368bc343bb6d989a32ade5fee09cee993feb72
JAvi <xgigante@gmail.com>
2024-11-19 13:08:06 +0100

    Initialize project with basic setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>The Walking Game.</p>
+    </div>
+  );
+};
+
+export default Home;
